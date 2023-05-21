b1e25528d6de6cde130f6d8d0c48bcce2190291a
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-05-21 14:32:03 +0600

    Create Coca-cola Store.cpp

---
+++ Coca-cola Store.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   int t ;
+   while(true){
+       cin>>t;
+       if(t==0) break;
+       cout<<t/2<<endl;
+   }
+    return 0;
+}
