e37a68e1435707503dc204c8739fb68972c9504c
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-06-01 00:26:51 +0600

    Create Trouble with a Pentagon.cpp

---
+++ Trouble with a Pentagon.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pi acos(-1)
+#define Sin(x) sin(x*pi/180.0)
+int main(){
+    double s;
+    while(cin>>s){
+         printf("%.10lf\n", Sin(108)/Sin(63)*s);
+        }
+        return 0;
+    }
