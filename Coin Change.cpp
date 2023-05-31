5c294c724b969b8c7a0ced2834ea473637fe5ee2
Shamura Ahmad <47661479+animshamura@users.noreply.github.com>
2023-05-31 17:34:43 +0600

    Create Coin Change.cpp

---
+++ Coin Change.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int coins[] = {50, 25, 10, 5, 1};
+int ways[8192];
+
+void SaveWays(){
+    ways[0] = 1;
+    for (int i = 0; i < 5; ++i)
+        for (int j = coins[i]; j < 8192; j++)
+             ways[j] += ways[j - coins[i]];
+}
+int main(){   
+    SaveWays();
+    int money;
+    while (cin >> money) cout << ways[money] << endl;        
+    return 0;
+}
